cb6a9d6365748d373f028713ca651df2c5f6d431
Vadym Korolov <60427025+korolov-vv@users.noreply.github.com>
2021-06-17 01:03:26 +0200

    Create AppInit class

--- src/main/java/ua/goit/config/DatabaseConnectionManager.java
+++ src/main/java/ua/goit/config/DatabaseConnectionManager.java
@@ -2,15 +2,34 @@ package ua.goit.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import ua.goit.util.PropertiesLoader;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Objects;
 
 public class DatabaseConnectionManager {
-    private HikariDataSource ds;
+    private static HikariDataSource ds;
+
+    public DatabaseConnectionManager() {
+    }
 
     public DatabaseConnectionManager(String host, String databaseName, String username, String password) {
         initDataSource(host, databaseName, username, password);
+    }
+
+    public static void init() {
+        PropertiesLoader propertiesLoader = new PropertiesLoader();
+        propertiesLoader.loadPropertiesFile("application.properties");
+        initDataSource(propertiesLoader);
+    }
+
+    public static synchronized HikariDataSource getDataSource() {
+        if (Objects.isNull(ds)) {
+            init();
+            return ds;
+        }
+        return ds;
     }
 
     public Connection getConnection() {
@@ -20,6 +39,18 @@ public class DatabaseConnectionManager {
             throwables.printStackTrace();
             throw new RuntimeException(throwables);
         }
+    }
+
+    private static void initDataSource(PropertiesLoader propertiesLoader) {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(String.format("jdbc:postgresql://%s/%s", propertiesLoader.getProperty("host"),
+                propertiesLoader.getProperty("database.name")));
+        config.setUsername(propertiesLoader.getProperty("username"));
+        config.setPassword(propertiesLoader.getProperty("password"));
+        config.setMaximumPoolSize(10);
+        config.setIdleTimeout(10_000);
+        config.setConnectionTimeout(10_000);
+        ds = new HikariDataSource(config);
     }
 
     private void initDataSource(String host, String databaseName, String username, String password) {
